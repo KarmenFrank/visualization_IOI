acb9c65712fa9700eca7911fbe67476cc686420d
MajBregar <maj.bregar@gmail.com>
2025-11-09 19:09:18 +0100

    Tourist data grouping + WIP timeline + WIP code cleanup next

---
+++ js/data_processing.js
@@ -0,0 +1,12 @@
+// async function loadTouristData() {
+//   const response = await fetch("./data/tourist_data_grouped.json");
+//   if (!response.ok) {
+//     throw new Error(`HTTP error ${response.status}`);
+//   }
+//   return await response.json();
+// }
+
+
+// // loadTouristData().then((data) => {
+// //     console.log("Data Loaded!");
+// // });
